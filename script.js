7ab0ca5e4074a979b3bc95a4ef31239a45cff688
Tobie Milford <tobie.milford@gmail.com>
2019-08-25 16:41:34 -0700

    #2: Image # displayer

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Create an app that lets users choose to display between 1 and 50 random dog images, 
-// then prints the results to the console. The app should feature a form with a required input where users 
-// indicate the number of images to retrieve, and the input should default to 3. Use the endpoint described 
-// in the "DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION" section of this page of the DogAPI docs.
+// Create an app that loads a single random image for a specific breed, based on a user input. This app should account for the happy case when the breed is found, as well as the unhappy case when it is not. Use the endpoint described in the "RANDOM IMAGE FROM A BREED COLLECTION" section of this page of the DogAPI docs. Note that the API will return an HTTP status code of 404 along with a JSON object with info about the error if a request is made for a breed that can't be found.
+
 $('.request_dog').submit(event => {
     event.preventDefault();
     let number = $('.number_input').val();
@@ -9,14 +7,19 @@ $('.request_dog').submit(event => {
 });
 
 function getImage(num) {
-
-    console.log(num.toString());
-
-    // let address = 'https://dog.ceo/api/breeds/image/random/';
-
-    fetch('https://dog.ceo/api/breeds/image/random/')
+    fetch('https://dog.ceo/api/breeds/image/random/' + num.toString())
         .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => displayImage(responseJson))
         .catch(error => alert('Something went wrong. Try again later.'));
 }
+
+function displayImage(json) {
+    console.log(json);
+    let image = json.message;
+    for (count = 0; count < image.length; count++) {
+        $('.dog_image').append(
+            `<img src="${image[count]}">`
+            ).css()
+        };
+    }
 
